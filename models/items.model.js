7a6627a57df575fa4c3a677f8ca64df215e2a08f
jesus1989garcia <jesus1989garcia@gmail.com>
2019-01-31 14:33:53 +0100

    feat: doCreate

--- models/items.model.js
+++ models/items.model.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose');
-const SALT_WORK_FACTOR = 10;
-//const FIRST_ADMIN_EMAIL = process.env.FIRST_ADMIN_EMAIL;
+
+
 
 const itemSchema = new mongoose.Schema({
   price: {
     type: Number,
-    required: 'Price is required'
+    required: 'true'
   },
   name: {
     type: String,
-    required: 'Name is required'
+    required: 'true'
   },
   category: {
     type: mongoose.Schema.Types.ObjectId, //esto tenemos que ver como funciona el desplegable
-    ref: 'Categories'
+    ref: 'true'
   },
   description: {
     type: String
@@ -22,37 +22,38 @@ const itemSchema = new mongoose.Schema({
     type: String
   },
   location: {
-      type: {type: String },
-      coordinates: [Number]
+    type: String  // hasta tengamos gmaps
+      // type: {type: String },
+      // coordinates: [Number]
   }, 
-  owner: {
-    type: ObjectId //revisar esta linea
-  }
+  // owner: {
+  //   type: ObjectId //revisar esta linea
+  // }
 }, { timestamps: true });
 
-itemSchema.pre('save', function(next) {
-    if (this.email === FIRST_ADMIN_EMAIL) {
-      this.role = constants.ROLE_ADMIN;
-    }
+// itemSchema.pre('save', function(next) {        esto sobra
+//     if (this.email === FIRST_ADMIN_EMAIL) {
+//       this.role = constants.ROLE_ADMIN;
+//     }
   
-    if (this.isModified('password')) {
-      bcrypt.genSalt(SALT_WORK_FACTOR)
-        .then(salt => {
-          return bcrypt.hash(this.password, salt)
-        })
-        .then(hash => {
-          this.password = hash;
-          next();
-        })
-        .catch(error => next(error));
-    } else {
-      next();
-    }
-  });
+//     if (this.isModified('password')) {
+//       bcrypt.genSalt(SALT_WORK_FACTOR)
+//         .then(salt => {
+//           return bcrypt.hash(this.password, salt)
+//         })
+//         .then(hash => {
+//           this.password = hash;
+//           next();
+//         })
+//         .catch(error => next(error));
+//     } else {
+//       next();
+//     }
+//   });
   
-  itemSchema.methods.checkPassword = function(password) {
-    return bcrypt.compare(password, this.password);
-  }
+//   itemSchema.methods.checkPassword = function(password) {
+//     return bcrypt.compare(password, this.password);
+//   }
   
   const Item = mongoose.model('Item', itemSchema);
   module.exports = Item;
