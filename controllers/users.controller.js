71a6700788ceb0852477cdd1e12616a3095ef2f0
sogchs <sogchs@gmail.com>
2019-02-06 20:09:02 +0100

    Search bar implemented by name

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -43,10 +43,24 @@ module.exports.editProfile = (req, res, next) => {
   console.info('DATA => ', req.file || 'default_pic')
   console.info('COSAS => ', req.params.id)
   
-  User.findByIdAndUpdate({ _id: req.params.id }, { alias, image: req.file.path.replace('public', '')}, { new: true }) // Revisar el new true
-    .then(user => {
-      console.log("profiled edited") 
-      res.redirect('/items' )
-    })
+User.findByIdAndUpdate({ _id: req.params.id }, { alias: req.body.alias, image: req.file.path.replace('public', '') })
+
+  // User.findById({ _id: req.params.id }, { alias: { "$exists" : false }})
+  // .then((user) => {
+  //   user.update({"$set": { alias: req.body.alias} })
+  // User.findById({ _id: req.params.id }, { image: { "$exists" : false }})
+  // .then((user) => {
+ 
+  //   user.update({"$set": { image: req.file.path.replace('public', '')} })
+  // User.findById(req.params.id)
+  //   .then((user) => {
+  //     // findByIdAndUpdate
+  //     user.set(req.body);
+  //     user.save()
+        .then((user) => {
+          console.log("profiled edited") 
+          res.redirect('/items' )
+        })
+    // })
     .catch(error => res.redirect('/user/list'))
-}
+  }
