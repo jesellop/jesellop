71ce49d8f59047ddb8ee1fe38d54c1187f106377
jesus1989garcia <jesus1989garcia@gmail.com>
2019-02-07 21:18:07 +0100

    fix: item-details-map, WIP: edit-item-map

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,73 @@
+// function initMap() {
+//   const domElement = document.getElementById("map");
+
+//   if (!domElement) { return; }
+
+//   window.map = new Map(domElement);
+//   window.map.init();
+
+//   if (navigator.geolocation)  {
+//     centerMapOnBrowser();
+//   }
+
+  
+//   if (document.getElementById("form-item" || "form-item-edit")) {  //here here
+//     window.map.onClick((event) => {
+    
+//       window.map.clearMarkers();
+      
+//       addMarkerAndUpateForm(event.latLng.lat(), event.latLng.lng());
+      
+//     })
+
+  
+//   } else if (document.getElementById("item-det")) {
+//     addItemMarkers();
+//   }
+// }
+
+// function addItemMarkers() {
+//   window.map.googleMap.setCenter({
+//     lat: introduce-lat , //introduce the right variables here!!!
+//     lng: introduce-lng
+//   });
+// }
+
+// function centerMapOnBrowser() {
+//   navigator.geolocation.getCurrentPosition((position) => {
+
+//     // Cambiad los valores de posicion en la vista!!
+//     const lat = document.getElementById('latitude').value 
+//     const lng = document.getElementById('longitude').value
+    
+//     window.map.googleMap.setCenter({
+//       lat: Number(lat) || position.coords.latitude,
+//       lng: Number(lng) || position.coords.longitude
+//     });
+
+//     window.map.googleMap.setZoom(18);
+
+    
+//     if ((document.getElementById("item-details")) || (document.getElementById("form-item-edit"))) {
+//       addMarkerAndUpateForm(Number(lat) || position.coords.latitude, Number(lng) || position.coords.longitude);
+//     }
+//   });
+// }
+
+// function addMarkerAndUpateForm(lat, lng) {
+//   window.map.addMarker(lat, lng);
+
+//   document.getElementById('latitude').value = lat;
+//   document.getElementById('longitude').value = lng;
+// }
+
+
+
+
+
+
+
+
 function initMap() {
   const domElement = document.getElementById("map");
 
@@ -6,20 +76,14 @@ function initMap() {
   window.map = new Map(domElement);
   window.map.init();
 
-  if (navigator.geolocation && document.getElementById("form-item"))  {
+  if (navigator.geolocation)  {
     centerMapOnBrowser();
   }
 
   
-  if (document.getElementById("form-item" || "form-item-edit")) {  //here here
+  if ((document.getElementById("form-item") || (document.getElementById("form-item-edit")))) {  //here here
     window.map.onClick((event) => {
-      // window.map.Circle({
-      //   strokeColor: '#FF0000',
-      //   strokeOpacity: 0.8,
-      //   strokeWeight: 2,
-      //   fillColor: '#FF0000',
-      //   fillOpacity: 0.35,
-      // map: map})
+    
       window.map.clearMarkers();
       
       addMarkerAndUpateForm(event.latLng.lat(), event.latLng.lng());
@@ -27,7 +91,7 @@ function initMap() {
     })
 
   
-  } else if (document.getElementById("item-details")) {
+  } else if (document.getElementById("item-det")) {
     addItemMarkers();
   }
 }
@@ -41,16 +105,21 @@ function addItemMarkers() {
 
 function centerMapOnBrowser() {
   navigator.geolocation.getCurrentPosition((position) => {
+
+    // Cambiad los valores de posicion en la vista!!
+    const lat = document.getElementById('latitude').value 
+    const lng = document.getElementById('longitude').value
+    
     window.map.googleMap.setCenter({
-      lat: position.coords.latitude,
-      lng: position.coords.longitude
+      lat: Number(lat) || position.coords.latitude,
+      lng: Number(lng) || position.coords.longitude
     });
 
     window.map.googleMap.setZoom(18);
 
     
-    if (document.getElementById("form-item-edit")) {
-      addMarkerAndUpateForm(position.coords.latitude, position.coords.longitude);
+    if ((document.getElementById("item-details")) || (document.getElementById("form-item-edit"))) {
+      addMarkerAndUpateForm(Number(lat) || position.coords.latitude, Number(lng) || position.coords.longitude);
     }
   });
 }
@@ -61,6 +130,3 @@ function addMarkerAndUpateForm(lat, lng) {
   document.getElementById('latitude').value = lat;
   document.getElementById('longitude').value = lng;
 }
-
-
-
